85dffac081afe89fa751b2700c460fe95adabc16
fierayan <fengyan314@gmail.com>
2013-09-05 21:33:38 +0800

    save id and get user image

--- popup.js
+++ popup.js
@@ -2,29 +2,15 @@ var userName,appDes,promoteReason,submitTime,appIconSrc, appName,screenShotSrc={
 var url="http://appshare.sinaapp.com/index.php/appdigger/like_obj";
 
 function gatherInformation(){
-    localStorage.clear();
-    userName=document.querySelector(".textarea_small").value;
+    userName=localStorage.getItem("userName");
     promoteReason=document.querySelector(".textarea_medium").value;
     appDes=document.querySelector(".textarea_large").value;
     submitTime=new Date();
     submitTime=submitTime.toISOString();
-    var pattern=/[a-z]{5,}/;
+
     if(pattern.test(userName)){
         if(promoteReason.length>=10){
             if(appDes.length>=20){
-                localStorage.setItem("userName",userName);
-                localStorage.setItem("promoteReason",promoteReason);
-                localStorage.setItem("appDes",appDes);
-                localStorage.setItem("submitTime",submitTime);
-                localStorage.setItem("appName",appName);
-                localStorage.setItem("appIconSrc",appIconSrc);
-                localStorage.setItem("shot_1",screenShotSrc[0]);
-                localStorage.setItem("shot_2",screenShotSrc[1]);
-                localStorage.setItem("shot_3",screenShotSrc[2]);
-                localStorage.setItem("shot_4",screenShotSrc[3]);
-                localStorage.setItem("appLink",appLink);
-                localStorage.setItem("appCategory",appCategory);
-
                 var info={
                     "userName": userName,
                     "submitTime": submitTime,
@@ -86,9 +72,31 @@ document.addEventListener('DOMContentLoaded', function (){
             screenShotSrc[2]=request.shot_3;
             screenShotSrc[3]=request.shot_4;
             sendResponse({farewell: "goodbye"});
-        });
+        }
+    );
     if(localStorage.getItem("userName")){
-        document.querySelector(".textarea_small").value=localStorage.getItem("userName");
+        document.querySelector(".textarea_small").parentNode.setAttribute("class","wrap hide");
+        document.querySelector(".userInfo").setAttribute("class","userInfo");
+        document.querySelector(".userInfo img").src="http://dayu.oa.com/avatars/"+localStorage.getItem("userName")+"/avatar.jpg";
+        document.querySelector(".userInfo span").innerHTML=localStorage.getItem("userName");
+    }else{
+        var once=document.querySelector(".textarea_small");
+        once.parentNode.setAttribute("class","wrap");
+        var always=document.querySelector(".userInfo");
+        always.setAttribute("class","userInfo  hide");
+        once.addEventListener("keypress",function(event){
+            if(event.keyCode===13){
+                var pattern=/[a-z]{5,}/;
+                if(pattern.test(event.target.value)){
+                    event.preventDefault();
+                    once.setAttribute("class","wrap hide");
+                    always.querySelector("img").src="http://dayu.oa.com/avatars/"+event.target.value+"/avatar.jpg";
+                    always.querySelector("span").innerHTML=event.target.value;
+                    always.setAttribute("class","userInfo");
+                    localStorage.setItem("userName",event.target.value);
+                }
+            }
+        },false);
     }
     var textareaAll=document.querySelectorAll(".textarea");
     for(var i=0;i<textareaAll.length;i++){
@@ -96,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function (){
             event.target.parentNode.querySelector(".warn").setAttribute("class","warn");
         },false);
         textareaAll[i].addEventListener("blur",function(event){
-            event.target.parentNode.querySelector(".warn").setAttribute("class","warn warn_hide");
+            event.target.parentNode.querySelector(".warn").setAttribute("class","warn hide");
         },false);
     }
     var subBtn=document.querySelector(".button");
